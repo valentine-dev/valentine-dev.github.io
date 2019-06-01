5acad5e64d0de2d8b95003d98d0a1d878c49a729
Valentine Wu <valentine.business@gmail.com>
2019-05-31 20:57:49 -0700

    Upload sample files

---
+++ react/sample1/precache-manifest.51f5283e681cd2b0bd95cbc5f7f4945f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5689521211c0fb2cbb6a5f1d1b975e4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f60f17cceeca811f8c9",
+    "url": "/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "8f60f17cceeca811f8c9",
+    "url": "/static/js/main.01c4109f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
