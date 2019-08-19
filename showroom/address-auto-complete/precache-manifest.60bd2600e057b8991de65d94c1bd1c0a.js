0bb2bef43675acd7d6cdf2b1a39c6df3ca5670ae
Valentine Wu <valentine.business@gmail.com>
2019-08-18 17:19:08 -0700

    version 0.6.0

---
+++ showroom/address-auto-complete/precache-manifest.60bd2600e057b8991de65d94c1bd1c0a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7b59a9cbff353078ad984498cfc5ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d56e067d8b4e4fda7adb",
+    "url": "./static/js/0.81110aa3.chunk.js"
+  },
+  {
+    "revision": "388c0565f80a7f9e0a90",
+    "url": "./static/js/3.ae2b21c7.chunk.js"
+  },
+  {
+    "revision": "460fde36af29daae1b4b",
+    "url": "./static/js/4.bf9b4156.chunk.js"
+  },
+  {
+    "revision": "758deae9223ca7b4fd71",
+    "url": "./static/js/5.9196e2c6.chunk.js"
+  },
+  {
+    "revision": "a11202363841025579a9",
+    "url": "./static/js/6.dbbcbda7.chunk.js"
+  },
+  {
+    "revision": "bec120cb1bd58450ed6d",
+    "url": "./static/js/7.6fabcc1d.chunk.js"
+  },
+  {
+    "revision": "97f6f9e9d7566a45c539",
+    "url": "./static/js/8.5dcffcdc.chunk.js"
+  },
+  {
+    "revision": "3f42826d6e044d251b70",
+    "url": "./static/js/main.cd0b166c.chunk.js"
+  },
+  {
+    "revision": "f514af897d985ac6926a",
+    "url": "./static/js/runtime~main.8beceb03.js"
+  }
+]);
